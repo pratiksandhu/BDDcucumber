490c3b1e26d763a9b32699c9f06925f0d29ddcec
Pratik Sandhu <pratiksandhu@pratiks-mbp.home>
2023-09-03 02:30:50 -0400

    updated config

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "515623ad-529d-4804-89cf-de8f0b48928e",
+    "id": "2401aa98-6299-46b3-a8e0-67d16e2e5345",
     "feature": "test afterSignInPage",
     "scenario": "test contacts page",
-    "start": 1693719764536,
+    "start": 1693722621947,
     "group": 17,
     "content": "",
     "tags": "",
-    "end": 1693719784943,
+    "end": 1693722641421,
     "className": "passed"
   },
   {
-    "id": "e078e941-76d6-4251-a906-409fb7063d1a",
+    "id": "531785cc-df25-403d-a33a-412dab45d6d6",
     "feature": "test afterSignInPage",
     "scenario": "test deals page",
-    "start": 1693719764537,
+    "start": 1693722621947,
     "group": 18,
     "content": "",
     "tags": "@deals,",
-    "end": 1693719779307,
+    "end": 1693722636183,
     "className": "failed"
   },
   {
-    "id": "b8b5e318-367b-467a-ac00-010d7fd62c6c",
+    "id": "728043b2-ca4e-4526-8a79-321631cc1c69",
     "feature": "Crm login",
     "scenario": "login into crm account",
-    "start": 1693719764561,
+    "start": 1693722621973,
     "group": 19,
     "content": "",
     "tags": "",
-    "end": 1693719769069,
+    "end": 1693722629545,
     "className": "passed"
   }
 ]);
