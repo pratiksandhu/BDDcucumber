7d0623fc44f92c5b41d44832d4ab85e28364433e
Pratik Sandhu <pratiksandhu@pratiks-mbp.home>
2023-09-04 01:18:58 -0400

    updated config

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,51 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"81e34726-ed5b-4805-a0e4-5a3717298d18","feature":"test afterSignInPage","scenario":"test deals page","start":1693725267376,"group":18,"content":"","tags":"@deals,","end":1693725281620,"className":"failed"},{"id":"e840ca2b-28bb-4ecc-8d18-57831e98a472","feature":"Crm login","scenario":"login into crm account","start":1693725267377,"group":19,"content":"","tags":"","end":1693725270859,"className":"passed"},{"id":"a4a894c1-e2e4-4690-b781-bc0c43b25f2d","feature":"test afterSignInPage","scenario":"test contacts page","start":1693725267377,"group":17,"content":"","tags":"","end":1693725287122,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":17,"content":"Thread[TestNG-PoolService-0,5,main]"},{"id":18,"content":"Thread[TestNG-PoolService-1,5,main]"},{"id":19,"content":"Thread[TestNG-PoolService-2,5,main]"}]);
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "d342dd1a-1bca-4c6c-9110-cfc336c18418",
+    "feature": "test afterSignInPage",
+    "scenario": "test contacts page",
+    "start": 1693804380963,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1693804401173,
+    "className": "passed"
+  },
+  {
+    "id": "15af7c7f-a738-4540-9914-34b48598c3a2",
+    "feature": "Crm login",
+    "scenario": "login into crm account",
+    "start": 1693804380987,
+    "group": 19,
+    "content": "",
+    "tags": "",
+    "end": 1693804385351,
+    "className": "passed"
+  },
+  {
+    "id": "ca3a4ab1-e042-4b1c-a123-858b0137326f",
+    "feature": "test afterSignInPage",
+    "scenario": "test deals page",
+    "start": 1693804380963,
+    "group": 18,
+    "content": "",
+    "tags": "@deals,",
+    "end": 1693804395808,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  }
+]);
 });
