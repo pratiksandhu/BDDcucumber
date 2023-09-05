7939f3ed6569981a84366d4e99dd0584d9d2545a
Pratik Sandhu <pratiksandhu6162@gmail.com>
2023-09-05 15:08:15 -0400

    updated

--- src/test/java/parallelExecution/Hooks.java
+++ src/test/java/parallelExecution/Hooks.java
@@ -58,45 +58,6 @@ public class Hooks {
 			byte[] sourcePath = ((TakesScreenshot)BaseClass.getDriver()).getScreenshotAs(OutputType.BYTES);
 			scenario.attach(sourcePath, "image/png", screenshotName);
 
-			// // Get the name of the scenario
-			// String scenarioName = scenario.getName().replaceAll(" ", "_");
-
-			// // Capture screenshot and get its path
-			// String screenshotPath = takeScreenshotAtEndOfTest(scenarioName);
-
-			// //encode image to base64.
-			// File file = new File(screenshotPath);
-			// byte[] fileContent = FileUtils.readFileToByteArray(file);
-			// String encodedString = Base64.getEncoder().encodeToString(fileContent);
-
-			// Add to your Spark Report (or extent report)
-			//ExtentCucumberAdapter.addTestStepLog("<img src='data:image/png;base64," + encodedString + "' />");
-
-			// Add to your cucumber report.
-			// byte[] screenshotBytes = ((TakesScreenshot)
-			// BaseClass.getDriver()).getScreenshotAs(OutputType.BYTES);
-			// scenario.attach(screenshotBytes, "image/png", scenarioName);
-			
-
-        // Add Base64 screenshot to Extent Reports
-       // ExtentCucumberAdapter.addTestStepLog("<img src='data:image/png;base64," + screenshotBase64 + "' />");
-
-		}
+}
 	}
-
-	// public String takeScreenshotAtEndOfTest(String scenarioName) throws IOException {
-	// 	File scrFile = ((TakesScreenshot) BaseClass.getDriver()).getScreenshotAs(OutputType.FILE);
-	// 	String currentDir = System.getProperty("user.dir");
-	// 	String screenshotDir = currentDir + "/test-output/SparkReport";
-	// 	String screenshotPath = screenshotDir + "/" + scenarioName + ".png"; // Unique name based on scenario
-
-	// 	File screenshotDirFile = new File(screenshotDir);
-	// 	if (!screenshotDirFile.exists()) {
-	// 		screenshotDirFile.mkdirs();
-	// 	}
-
-	// 	FileUtils.copyFile(scrFile, new File(screenshotPath));
-	// 	return screenshotPath;
-	// }
-
 }
