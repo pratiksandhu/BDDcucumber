116d2b971da53c4d5b26f54c420c5104f1dfc1e1
Pratik Sandhu <pratiksandhu6162@gmail.com>
2023-09-07 13:23:15 -0400

    updated config

---
+++ test-output-thread/report.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "19f577cd-a2e2-485b-ae04-7d8e4e783444",
+    "feature": "test afterSignInPage",
+    "scenario": "test deals page",
+    "start": 1694095103435,
+    "group": 18,
+    "content": "",
+    "tags": "@deals,",
+    "end": 1694095118482,
+    "className": "failed"
+  },
+  {
+    "id": "c0de42fd-fddf-4e13-a540-f78f336294cf",
+    "feature": "Crm login",
+    "scenario": "login into crm account",
+    "start": 1694095103459,
+    "group": 19,
+    "content": "",
+    "tags": "",
+    "end": 1694095108402,
+    "className": "passed"
+  },
+  {
+    "id": "1bcff668-f32d-45d4-b366-1191217ded31",
+    "feature": "test afterSignInPage",
+    "scenario": "test contacts page",
+    "start": 1694095103435,
+    "group": 17,
+    "content": "",
+    "tags": "",
+    "end": 1694095124825,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  }
+]);
+});
