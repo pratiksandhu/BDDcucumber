1fe14d11e25594ec7122f8b546d169418b529a96
Pratik Sandhu <pratiksandhu6162@gmail.com>
2023-09-07 23:54:14 -0400

    updated config

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "19f577cd-a2e2-485b-ae04-7d8e4e783444",
-    "feature": "test afterSignInPage",
-    "scenario": "test deals page",
-    "start": 1694095103435,
-    "group": 18,
-    "content": "",
-    "tags": "@deals,",
-    "end": 1694095118482,
-    "className": "failed"
-  },
-  {
-    "id": "c0de42fd-fddf-4e13-a540-f78f336294cf",
-    "feature": "Crm login",
-    "scenario": "login into crm account",
-    "start": 1694095103459,
-    "group": 19,
-    "content": "",
-    "tags": "",
-    "end": 1694095108402,
-    "className": "passed"
-  },
-  {
-    "id": "1bcff668-f32d-45d4-b366-1191217ded31",
+    "id": "266ee009-508c-42f3-82ef-b684c587d6f3",
     "feature": "test afterSignInPage",
     "scenario": "test contacts page",
-    "start": 1694095103435,
+    "start": 1694144395992,
     "group": 17,
     "content": "",
     "tags": "",
-    "end": 1694095124825,
+    "end": 1694144416697,
+    "className": "failed"
+  },
+  {
+    "id": "00700023-0dcd-4144-9641-f813dd52da76",
+    "feature": "Crm login",
+    "scenario": "login into crm account",
+    "start": 1694144396017,
+    "group": 19,
+    "content": "",
+    "tags": "",
+    "end": 1694144400235,
+    "className": "passed"
+  },
+  {
+    "id": "63d4b022-8ac9-4061-8a7b-47e66f53e4e9",
+    "feature": "test afterSignInPage",
+    "scenario": "test deals page",
+    "start": 1694144395992,
+    "group": 18,
+    "content": "",
+    "tags": "@deals,",
+    "end": 1694144410856,
     "className": "failed"
   }
 ]);
