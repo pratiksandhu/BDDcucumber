990138f053aa7cc94d017f9668565aa3244eb71d
Pratik Sandhu <pratiksandhu6162@gmail.com>
2023-09-21 00:02:16 -0400

    updated

--- run_cucumber_tests.sh
+++ run_cucumber_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-feature_file=$1
-mvn test -Dbrowser=${TEST_BROWSER} -Dcucumber.features="$(feature_file)"  -Dcucumber.filter.tags="@Regression or @Smoke" -Dcucumber.plugin="json:target/cucumber-report.json,junit:target/cucumber-report.xml"
+feature_file="$1"
+mvn test -Dbrowser=${TEST_BROWSER} -Dcucumber.features="$feature_file"  -Dcucumber.filter.tags="@Regression or @Smoke" -Dcucumber.plugin="json:target/cucumber-report.json,junit:target/cucumber-report.xml"
